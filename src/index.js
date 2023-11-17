16887851be369d08d38715c7edcec033367d04d7
Rajendar <raa.singh0000@gmail.com>
2023-11-17 06:18:56 +0530

    changes for deploy and error page 404 added

--- src/index.js
+++ src/index.js
@@ -1,49 +1,47 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Data/store";
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-} from "react-router-dom";
 import HomePageLayout from "./Layouts/HomePageLayout";
+import "./App.css";
 import Home from "./pages/Home";
 import Exercises from "./pages/Exercises";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import "./App.css";
 import Dashboard from "./pages/Dashboard";
 import DashboardLayout from "./Layouts/DashboardLayout";
 import Profile from "./pages/Profile";
 import StartWorkout from "./pages/StartWorkout";
 import History from "./pages/History";
 import EditProfile from "./pages/EditProfile";
+import NotFound from "./pages/NotFound";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/" element={<HomePageLayout />}>
-        <Route path="" element={<Home />} />
-        <Route path="Login" element={<Login />} />
-        <Route path="Exercises" element={<Exercises />} />
-        <Route path="Register" element={<Register />} />
-      </Route>
-      <Route path="/Dashboard" element={<DashboardLayout />}>
-        <Route path="" element={<Dashboard />} />
-        <Route path="EditProfile" element={<EditProfile />} />
-        <Route path="Profile" element={<Profile />} />
-        <Route path="StartWorkout" element={<StartWorkout />} />
-        <Route path="History" element={<History />} />
-        <Route path="Exercises" element={<Exercises />} />
-      </Route>
-    </>
-  )
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <Provider store={store}>
-    <RouterProvider router={router} />
-  </Provider>
+    <Router basename="/workout-tracker">
+      <Routes>
+        {/* Your existing routes */}
+        <Route path="/" element={<HomePageLayout />}>
+          <Route index element={<Home />} />
+          <Route path="Login" element={<Login />} />
+          <Route path="Exercises" element={<Exercises />} />
+          <Route path="Register" element={<Register />} />
+        </Route>
+
+        <Route path="/Dashboard" element={<DashboardLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="EditProfile" element={<EditProfile />} />
+          <Route path="Profile" element={<Profile />} />
+          <Route path="StartWorkout" element={<StartWorkout />} />
+          <Route path="History" element={<History />} />
+          <Route path="Exercises" element={<Exercises />} />
+        </Route>
+
+        {/* Catch-all route for custom 404 page */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
